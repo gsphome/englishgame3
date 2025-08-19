c256159a09dc2c1204c63579f6a9ce7841b374aa
Genil Suarez <genil.suarez@gmail.com>
2025-08-18 22:18:58 -0400

    feat: Fix ESLint no-unused-vars warning for optionsContainer

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'no-unused-vars': ['warn', { 'varsIgnorePattern': '^(icon|description|feedbackContainer|MESSAGES|learningModules|auth)$' }]
+    'no-unused-vars': ['warn', { 'varsIgnorePattern': '^(icon|description|feedbackContainer|MESSAGES|learningModules|auth|optionsContainer)$' }]
   },
   globals: {
     MESSAGES: 'readonly',
